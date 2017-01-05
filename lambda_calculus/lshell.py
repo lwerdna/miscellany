9b76a0e92307b8aff3c03e05942dd9790ec5e32a
Andrew Lamoureux <andrew@vector35.com>
2017-01-05 13:20:15 -0500

    lambda calculus: token manager

--- lambda_calculus/lshell.py
+++ lambda_calculus/lshell.py
@@ -12,12 +12,40 @@ class TID:
 class Token:
 	def __init__(self,ident,val=None):
 		self.ident, self.val = ident, val
+
 	def __str__(self):
 		if self.ident == TID.LAMBDA: return "LAMBDA"
 		if self.ident == TID.DOT: return "DOT"
 		if self.ident == TID.SPACE: return "SPACE"
 		if self.ident == TID.LCID: return "LCID\"%s\"" % self.val
 		raise("unknown token id: %d" % self.ident)
+
+class TokenManager:
+	def __init__(self, tokenList):
+		self.tokenList = tokenList
+		self.i = 0
+
+	def reset(self):
+		self.i = 0
+
+	def peek(self, nAhead=0):
+		if (self.i + nAhead) >= len(self.tokenList):
+			return None
+		return self.tokenList[self.i + nAhead]
+	
+	def consume(self):
+		if (self.i) >= len(self.tokenList):
+			return None
+		return self.tokenList[self.i]
+
+	def isDone(self):
+		return self.peek() == None
+
+	def __str__(self):
+		result = []
+		for k in range(self.i, len(self.tokenList)):
+			result.append("%d: %s" % (k, str(self.tokenList[k])))
+		return "\n".join(result)
 
 def tokenize(line):
 	tokens = []
@@ -46,17 +74,38 @@ def tokenize(line):
 		else:
 			raise("tokenizing on \"%s...\"" % chars[i:i+8])
 
-	return tokens
+	return TokenManager(tokens)
 
 ###############################################################################
 # PARSING
 ###############################################################################
+
+class Term:
+	def __init__(self):
+		pass
+
+class Variable(Term):
+	def __init__(self, name):
+		self.name = name
+
+class Abstraction(Term):
+	def __init__(self, variable, term):
+		self.variable = variable
+		self.term = term
+
+class Application(Term):
+	def __init__(self, termA, termB):
+		self.termA = termA
+		self.termB = termB
+
+def parse(tokens):
+	pass
 
 ###############################################################################
 # MAIN
 ###############################################################################
 if __name__ == '__main__':
 	for line in sys.stdin:
-		tokens = tokenize(line)
+		tokenMgr = tokenize(line)
 
-		print '\n'.join(map(str, tokens))
+		print tokenMgr
