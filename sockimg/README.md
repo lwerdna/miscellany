4c13d27d877f81987dbd20ca6936a6cfd1686c32
Andrew Lamoureux <andrew@vector35.com>
2017-10-27 19:26:50 -0400

    sockimg: display images written to socket

---
+++ sockimg/README.md
@@ -0,0 +1,5 @@
+displays images written to a unix socket
+
+useful for live editing some programmatic generation of images
+
+test with: `cat foo.png | nc localhost 31337`
---
+++ sockimg/sockimg.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# display images written to unix socket
+
+import io, os, sys, time, select, socket
+import binascii
+
+import Tkinter
+
+from PIL import Image, ImageTk
+
+svr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+svr.bind(('localhost', 31337))
+svr.listen(1)
+
+tk = Tkinter.Tk()
+
+label = None
+
+while 1:
+	tk.update_idletasks()
+	tk.update()
+
+	fdset_r = [svr]
+	fdset_r, fdset_w, fdset_e = select.select(fdset_r, [], [], .001)
+	if svr in fdset_r:
+		cli, addr = svr.accept()
+		data = ''
+		while 1:
+			tmp = cli.recv(4096)
+			if not tmp:
+				break;
+			data += tmp
+		cli.close()
+		print "received %d bytes %s...%s" % (len(data), binascii.hexlify(data[0:16]), binascii.hexlify(data[-16:]))
+
+		if label:
+			label.destroy()
+	
+		try:	
+			image = Image.open(io.BytesIO(data))
+			image = image.convert('RGB') # else it won't draw PNG's with transparency
+			photo = ImageTk.PhotoImage(image)
+
+			label = Tkinter.Label(tk, image=photo)
+			label.image = photo
+			label.pack()
+		except Exception:
+			pass
