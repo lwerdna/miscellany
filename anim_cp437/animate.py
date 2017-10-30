331652d41591214a52f649b06023a483e1c96f5a
Andrew Lamoureux <andrew@vector35.com>
2017-10-30 11:36:46 -0400

    cp437 animate tweaks

--- anim_cp437/animate.py
+++ anim_cp437/animate.py
@@ -3,21 +3,37 @@
 import os
 import subprocess
 from cp437 import getImgLine, getImgString
+from PIL import Image, ImageDraw
 
+bgcolor = (255, 255, 255)
 delay = 100 # in 100'ths of second
+margin = 4
 
 def setDelay(delay_):
 	global delay
 	delay = delay_
 
+def setMargin(margin_):
+	global margin
+	margin = margin_
+
 def write(framesTxt, fnameOut):
-	global delay
+	global delay, margin, bgcolor
 
 	# convert text frames -> image frames
 	framesImg = []
 	for f in framesTxt:
 		framesImg.append(getImgString(f))
-	
+
+	# add margins
+	tmp = []
+	for fi in framesImg:
+		(width, height) = fi.size
+		imgMargin = Image.new('RGB', (width+2*margin, height+2*margin), bgcolor)
+		imgMargin.paste(fi, (margin, margin))
+		tmp.append(imgMargin)
+	framesImg = tmp
+
 	# map images frames -> file names
 	framesFname = []
 	for (i, img) in enumerate(framesImg):
