94240fba7ea6789e9d0a1a3e617a96a7b63609fd
Andrew Lamoureux <andrew@vector35.com>
2020-04-17 11:28:04 -0400

    Update replace_in_files

--- replace_in_files.py
+++ replace_in_files.py
@@ -1,40 +1,47 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-# search and replace, in this example:
-# * in all Makefiles
-# * excluding subdirectory called "./arch/"
+# step1: modify this to apply to files of interest, and before/after string
+# 	gvim `which replace_in_files`
+#
+# step2: run to make sure it hits what you think
+# 	replace_in_files
+#
+# step3: backup, run with 'really' to make modifications
+# 	replace_in_files really
 
 import re
 import os
+import sys
 
 for root, dirs, files in os.walk('.'):
-    for fname in files:
-        #if not re.match(r'^Makefile.*$', fname):
-        #    continue
-        if not (fname.startswith('ex') and fname.endswith('.html')):
-        	continue
+	for fname in files:
+		#if not re.match(r'^Makefile.*$', fname):
+		#	continue
+		if not fname.endswith('.py'):
+			continue
 
-        #if (root != '.') and (root[0:7] != './arch/'):
-        #    continue
+		#if (root != '.') and (root[0:7] != './arch/'):
+		#	continue
 
-        fpath = os.path.join(root, fname)
-        #print "root is: %s" % root
-        print("opening %s" % fpath)
-        fp = open(fpath, 'r+')
-        stuff = fp.read()
+		fpath = os.path.join(root, fname)
+		#print "root is: %s" % root
+		print("opening %s" % fpath)
+		fp = open(fpath, 'r+')
+		stuff = fp.read()
 
-        hits = re.findall(r'\.\.\/javascript\/', stuff)
-        #hits2 = re.findall(r'\s-O3', stuff)
-        total = len(hits)
-        if not total:
-            fp.close()
-            continue
+		hits = re.findall(r'usr/bin/env python', stuff)
+		#hits2 = re.findall(r'\s-O3', stuff)
+		total = len(hits)
+		if not total:
+			fp.close()
+			continue
 
-        print("replacing %d instances" % total)
+		print("replacing %d instances" % total)
 
-        stuff = re.sub(r'\.\.\/javascript\/', r'', stuff)
+		if sys.argv[1:] and sys.argv[1] == 'really':
+			stuff = re.sub(r'env python', r'env python3', stuff)
 
-        fp.seek(0)
-        fp.write(stuff)
-        fp.close()
+		fp.seek(0)
+		fp.write(stuff)
+		fp.close()
 
