676f5aa4d67935fac64affc3b51dc6b0362e5145
Andrew Lamoureux <andrew@vector35.com>
2020-04-17 12:32:42 -0400

    Update mdtags: options to list files, etc.

--- mdtags.py
+++ mdtags.py
@@ -10,18 +10,44 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(this_dir)
 import taglib
 
-exceptions = ['#print']
-
+# collect tag data
+exceptions = ['#print', '#if']
 tag2count = {}
+tag2files = {}
 for fname in filter(lambda x: x.endswith('.md'), os.listdir('.')):
 	with open(fname, 'r') as fp:
 		lines = fp.readlines()
 
 	for line in lines:
-		for tag in re.findall(r'#[a-zA-Z]\w*', line[1:]):
+		if not re.match(r'^\s*TAGS:', line):
+			continue
+
+		for tag in re.findall(r'#[a-zA-Z]\w*', line):
 			if tag in exceptions: continue
 			tag2count[tag] = tag2count.get(tag, 0) + 1
+			tag2files[tag] = tag2files.get(tag, []) + [fname]
 
-for t in sorted(tag2count, key=lambda x: tag2count[x]):
-	print('%s %s%s\x1B[0m' % (str(tag2count[t]).rjust(4), taglib.tag_to_color(t), t))
+# obey user command
+arg = sys.argv[1] if sys.argv[1:] else 'count'
+print('arg is: ', arg)
+
+if arg == 'count':
+	for t in sorted(tag2count, key=lambda x: tag2count[x]):
+		print('%s %s%s\x1B[0m' % (str(tag2count[t]).rjust(4), taglib.tag_to_color(t), t))
+
+elif arg == '-l':
+	for t in sorted(tag2count, key=lambda x: tag2count[x]):
+		print('%s %s%s\x1B[0m' % (str(tag2count[t]).rjust(4), taglib.tag_to_color(t), t))
+		for fname in tag2files[tag]:
+			print('\t%s' % fname)
+
+else:
+	tag = '#'+arg
+	if not tag in tag2count:
+		print('no files have that tag')
+		sys.exit(0)
+
+	print('%s %s%s\x1B[0m' % (str(tag2count[tag]).rjust(4), taglib.tag_to_color(tag), tag))
+	for fname in tag2files[tag]:
+		print('\t%s' % fname)
 
