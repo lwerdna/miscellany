0af5a2306eacf4d782a6ed15c93584f48f3af3a8
Andrew Lamoureux <andrew@vector35.com>
2020-05-30 16:02:48 -0400

    vimmv: add 'git' switch to mv's will be git mv's

--- vimmv.py
+++ vimmv.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+# presents a text file within vim for mass renaming
+# so you can run regexes, etc.
+
 import os
+import sys
 import os.path
 import tempfile
 import subprocess
@@ -8,6 +12,8 @@ import subprocess
 RED = '\x1B[31m'
 GREEN = '\x1B[32m'
 NORMAL = '\x1B[0m'
+
+git_mode = sys.argv[1:] and sys.argv[1]=='git'
 
 ignore = ['.DS_Store']
 fnames = [x for x in os.listdir('.') if os.path.isfile(x) and not x in ignore]
@@ -33,4 +39,7 @@ for i in range(len(fnames)):
 		continue
 
 	print('renaming %s%s%s -> %s%s%s' % (RED, before, NORMAL, GREEN, after, NORMAL))
-	os.rename(before, after)
+	if git_mode:
+		os.system('git mv \'%s\' \'%s\'' % (before, after))
+	else:
+		os.rename(before, after)
